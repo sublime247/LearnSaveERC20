54f201a97fc65f688bb29cd30ff9725ba37fd76d
sublime247 <kushimobashir2000@gmail.com>
2024-08-30 03:12:28 +0100

    LearningERC20

---
+++ contracts/BSKToken.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BSKToken is ERC20("BSKToken", "BSK"){
+    address public owner;
+
+
+  constructor(){
+    owner=msg.sender;
+    _mint(msg.sender, 200000e18);
+  }
+
+function mint(uint _amount ) external{
+    require(msg.sender==owner, "You are not authorize to mint new Token");
+
+    _mint(msg.sender, _amount * 1e18);
+}
+}
